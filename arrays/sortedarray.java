2f003ecac478ec2b9898cbe1f095170d6de0f493
lokesh <lokesh.77664@gmail.com>
2026-08-19 19:52:44 +0530

    sorted and rotationt

---
+++ arrays/sortedarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for(int i = 0 ; i < n ; i++){
+            if( nums[i] != 0){
+                nums[count] = nums[i];
+                count++;
+           
+            }
+            
+
+           } 
+           
+           while(count < n){
+            nums[count ++] = 0;
+        }
+
+    }
+}
+
+     
+        }
+        return (deviation <= 1) ? true : false ; 
+    }
+
+}
