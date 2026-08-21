73daf6edef3791ab3bab0c895a20d044ee816cd3
lokesh <lokesh.77664@gmail.com>
2026-08-21 18:26:50 +0530

    better approach

--- arrays/singlenumber.java
+++ arrays/singlenumber.java
@@ -1,18 +1,28 @@
-class Solution {
-    public int findMaxConsecutiveOnes(int[] nums) {
-        int n = nums.length ;
-int count = 0 ;
-int maxi = 0;
-    for( int i = 0 ;  i < n  ; i++){
+// class Solution {
+//     public int findMaxConsecutiveOnes(int[] nums) {
+//         int n = nums.length ;
+// int count = 0 ;
+// int maxi = 0;
+//     for( int i = 0 ;  i < n  ; i++){
         
-if(nums[i] == 1 ){
-  count++;
-}
-else{
-    count = 0 ;
-}
-maxi = Math.max(maxi,count);
-    }
-return maxi;
+// if(nums[i] == 1 ){
+//   count++;
+// }
+// else{
+//     count = 0 ;
+// }
+// maxi = Math.max(maxi,count);
+//     }
+// return maxi;
+//     }
+// }
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int Xorr = 0 ;
+        for(int arr : nums){
+            Xorr ^= arr;
+        }
+        return Xorr;
     }
 }
